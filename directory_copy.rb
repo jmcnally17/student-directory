c95e1801c221414e81634b20a9b2231399391975
Joe McNally <joe.t.mcnally@hotmail.com>
2022-03-08 22:44:25 +0000

    Exercise 6: centering, completed

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,23 +1,18 @@
+# 40 was chosen as the paramter for each use of the center method as that is just above
+# the longest output message
 # Let's put all students into an array of hashes
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students".center(40)
+  puts "To finish, just hit return twice".center(40)
   # create and empty array
   students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
-    # ask for the students hobby, height in cm and what universe they are from
-    puts "What is their hobby?"
-    hobby = gets.chomp
-    puts "What is their height in cm?"
-    height = gets.chomp.to_f
-    puts "What universe are they from?"
-    universe = gets.chomp
     # add the student hash to the array
-    students << {name: name, cohort: :november, hobby: hobby, height: height, universe: universe}
-    puts "Now we have #{students.length} students"
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.length} students".center(40)
     # get another name from the user
     name = gets.chomp
   end
@@ -26,18 +21,18 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(40)
+  puts "-------------".center(40)
 end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]}, hobby: #{student[:hobby]}, height: #{student[:height]}cm, universe: #{student[:universe]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(40)
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.length} great students"
+  puts "Overall, we have #{names.length} great students".center(40)
 end
 
 # Nothing happens until we call the methods
