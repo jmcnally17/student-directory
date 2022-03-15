1be92cb74736145758541a6b32819213574d4edb
Joe McNally <joe.t.mcnally@hotmail.com>
2022-03-15 15:40:59 +0000

    Step 14: Exercise 8 completed. Created a method as an example of a slightly more complex self reading file

--- self_read.rb
+++ self_read.rb
@@ -1,3 +1,8 @@
-# File.read is used to read the source code of a particular file. Combined with
-# __FILE__ which will read the file currently being executed.
-puts File.read(__FILE__)
+def self_read
+  # File.read is used to read the source code of a particular file. Combined with
+  # __FILE__ which will read the file currently being executed.
+  puts File.read(__FILE__)
+end
+
+# need to call the method
+self_read
